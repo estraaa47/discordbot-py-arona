abb7a61b519de8d37f2adbe9397516837e46c8e1
estraaa47 <kimjihoon683@gmail.com>
2026-02-23 06:56:32 +0900

    feat: Add new Arona cog for bot functionality.

--- cogs/arona.py
+++ cogs/arona.py
@@ -46,7 +46,7 @@ class AronaChat(commands.Cog):
         for cid in to_delete:
             del self.channel_memory[cid]
 
-    @app_commands.command(name="Arona", description="Chat with Arona")
+    @app_commands.command(name="arona", description="Chat with Arona")
     async def arona_slash(self, interaction: discord.Interaction, message: str):
         await interaction.response.defer()
         reply = await self.get_arona_reply(interaction.channel.id, interaction.user.display_name, message)
